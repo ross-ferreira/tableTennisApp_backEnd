4b22fd876fb65acee0614bdf7bb3602e6d944c70
Ross Ferreira <ross.p.ferreira@gmail.com>
2020-03-30 09:44:42 +0100

    Added Eloquent Models for Score and Player

--- app/Player.php
+++ app/Player.php
@@ -7,5 +7,10 @@ use Illuminate\Database\Eloquent\Model;
 class Player extends Model
 {
     protected $fillable = ['playerName', 'gamesWon', 'totalPoints', 'gamesPlayed'];
+
+    public function score(){
+        // a comment belongs to an article
+        return $this->belongsTo(Score::class); 
+    }
 }
 
--- app/Score.php
+++ app/Score.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Score extends Model
 {
-    // protected $fillable = ['round', 'player1_id', 'scoreP1', 'resultP1','player2_id', 'scoreP2', 'resultP2'];
-
+    protected $fillable = ['round', 'player1_id', 'scoreP1', 'resultP1','player2_id', 'scoreP2', 'resultP2'];
+    
+    public function players(){
+        // use hasMany relationship method
+        return $this->hasMany(Player::class); 
+    }
 
 }
